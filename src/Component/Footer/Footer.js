35ca3a33c9f825d7cab89cf76f8626627286a77c
ayoubkhan123 <ayubkhan666304@gmail.com>
2022-01-01 22:10:32 -0800

    webloop test

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,11 +3,9 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className="container8">
-        <div className="head3">
-        <h1>FREQUENTLY ASKED QUESTIONS</h1>
-        <p>You have questions, and we have answers. Couldn't find an anwser? Hop in our Discord Community!</p>
-
+        <div className="container11">
+        <div className="head11">
+       
         </div>
             
 
